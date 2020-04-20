d81991503c51d5a2f5697c658012a1f526d8625b
kelz <kelsie.langston@yahoo.com>
2020-04-19 22:48:20 -0400

    static folder points to build folder

--- server.js
+++ server.js
@@ -24,14 +24,20 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  //for when connected in Heroku
+  app.use(express.static("client/build"));
+}
+
 //api route
 app.use("/api", apiRoutes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
